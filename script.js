9b014518e38184458ac26032a218191f2044e420
Aaadarsh <58807249+hupu496@users.noreply.github.com>
2020-11-15 17:20:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var a=prompt("Enter UserName");
  var b=prompt("Enter password");
   	if(a== "swetu" && b=="19-11-2020")
   	{
-  	    window.location.href="/birthday";
+  	    window.location.href="start.html";
   	}
   	else
   	{
